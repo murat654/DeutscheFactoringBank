d0f3ec9a374d619bfcce14957293e1d456a71b41
Murat <muratdurmaz654@gmail.com>
2023-07-26 13:26:00 +0200

    FailedTestRunner is updated

--- src/test/java/com/dfb/runners/FailedTestRunner.java
+++ src/test/java/com/dfb/runners/FailedTestRunner.java
@@ -7,7 +7,7 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = "@target/rerun.txt",
-        glue = "com/jobbooster/step_definitions"
+        glue = "com/dfb/step_definitions"
 )
 public class FailedTestRunner {
 }
